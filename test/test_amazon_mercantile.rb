20fde9431765d84e3055eff5faceba4dcab97256
James Thompson <jamest@thereadyproject.com>
2013-07-17 16:38:52 -0600

    Added additional tests for AmazonMercantile.establish_connection.

--- test/test_amazon_mercantile.rb
+++ test/test_amazon_mercantile.rb
@@ -1,17 +1,43 @@
 require 'minitest_helper'
 
 describe AmazonMercantile do
-  let(:valid_options) {
-    {
-      merchant_id:    '12345SAMPLE',
-      marketplace_id: '12345SAMPLE',
-      aws_access_key: '12345SAMPLE',
-      aws_secret_key: '12345SAMPLE'
+  before :each do
+    @valid_options = {
+        merchant_id:    '12345SAMPLE',
+        marketplace_id: '12345SAMPLE',
+        aws_access_key: '12345SAMPLE',
+        aws_secret_key: '12345SAMPLE'
     }
-  }
+  end
 
   describe '.establish_connection' do
-    it { AmazonMercantile.establish_connection(valid_options).must_be_kind_of AmazonMercantile::Connection }
+    it { AmazonMercantile.establish_connection(@valid_options).must_be_kind_of AmazonMercantile::Connection }
+
+    describe 'invalid options' do
+      it { lambda{AmazonMercantile.establish_connection({
+        marketplace_id: '12345SAMPLE',
+        aws_access_key: '12345SAMPLE',
+        aws_secret_key: '12345SAMPLE'
+      })}.must_raise ArgumentError}
+
+      it { lambda{AmazonMercantile.establish_connection({
+        merchant_id:    '12345SAMPLE',
+        aws_access_key: '12345SAMPLE',
+        aws_secret_key: '12345SAMPLE'
+      })}.must_raise ArgumentError}
+
+      it { lambda{AmazonMercantile.establish_connection({
+        merchant_id:    '12345SAMPLE',
+        marketplace_id: '12345SAMPLE',
+        aws_secret_key: '12345SAMPLE'
+      })}.must_raise ArgumentError}
+
+      it { lambda{AmazonMercantile.establish_connection({
+        merchant_id:    '12345SAMPLE',
+        marketplace_id: '12345SAMPLE',
+        aws_access_key: '12345SAMPLE',
+      })}.must_raise ArgumentError}
+    end
   end
 
   describe '.connection' do
